737057d0fd57181e9cd36dc1466048a2572001f8
Daniel Wilk <wilkdaniel03@gmail.com>
2025-10-15 16:52:26 +0200

    [api] configuring webserver

--- api/main.py
+++ api/main.py
@@ -1,5 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
 def main():
-    print("Hello from api!")
+    return {"status":"OK"}
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app",port=8080,reload=True)
