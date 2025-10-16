eb7abd595368a3075e2db687e0b0a0e27fbb101e
Daniel Wilk <wilkdaniel03@gmail.com>
2025-10-16 14:51:58 +0200

    [web] conditional displaying fetched data

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -9,6 +9,12 @@ interface ITransport {
 	reservation: string;
 }
 
+enum FetchStatus {
+	Pending = 0,
+	Success = 1,
+	Failed = 2
+}
+
 const fetchTransports = async () => {
 	const headers = new Headers();
 	headers.append("Accept","application/json");
@@ -16,42 +22,57 @@ const fetchTransports = async () => {
 	const res = await fetch("http://127.0.0.1:8081/transport",{method:"GET",headers:headers});
 	const data  = await res.json();
 	return data;
-};
+}
 
 const DashboardPage = () => {
+	let [status,setStatus] = useState<FetchStatus>(FetchStatus.Pending);
 	let [transportData,setTransportData] = useState<ITransport[]>([]);
 
 	useEffect(() => {
 		fetchTransports().then((res) => {
 			setTransportData(res['data']);
+			setStatus(FetchStatus.Success);
+		}).catch(_ => {
+			setStatus(FetchStatus.Failed);
 		});
 	},[]);
 
-	console.log(transportData);
+	const renderRow = (data: ITransport) => {
+		if(status == FetchStatus.Success) {
+			return (
+				<chakra.TableRow key={data.id} _hover={{bg:"gray.100"}}>
+					<chakra.TableCell>{data.name}</chakra.TableCell>
+					<chakra.TableCell>{data.type}</chakra.TableCell>
+					<chakra.TableCell>{data.reservation}</chakra.TableCell>
+				</chakra.TableRow>
+			);
+		}
+	}
 
 	return (
 		<>
 			<Heading>Panel Administracyjny</Heading>
-			<chakra.TableRoot size="md">
-				<chakra.TableHeader>
-					<chakra.TableRow>
-						<chakra.TableCell><chakra.Text fontWeight="bold">Nazwa</chakra.Text></chakra.TableCell>
-						<chakra.TableCell><chakra.Text fontWeight="bold">Typ</chakra.Text></chakra.TableCell>
-						<chakra.TableCell><chakra.Text fontWeight="bold">Rezerwacja</chakra.Text></chakra.TableCell>
-					</chakra.TableRow>
-				</chakra.TableHeader>
-				<chakra.TableBody>
-					{transportData.map((obj,i) => {
-						return (
-							<chakra.TableRow key={i} _hover={{bg:"gray.100"}}>
-								<chakra.TableCell>{obj.name}</chakra.TableCell>
-								<chakra.TableCell>{obj.type}</chakra.TableCell>
-								<chakra.TableCell>{obj.reservation}</chakra.TableCell>
-							</chakra.TableRow>
-						);
-					})}
-				</chakra.TableBody>
-			</chakra.TableRoot>
+			{status != FetchStatus.Failed ? (
+				<chakra.TableRoot size="md">
+					<chakra.TableHeader>
+						<chakra.TableRow>
+							<chakra.TableCell><chakra.Text fontWeight="bold">Nazwa</chakra.Text></chakra.TableCell>
+							<chakra.TableCell><chakra.Text fontWeight="bold">Typ</chakra.Text></chakra.TableCell>
+							<chakra.TableCell><chakra.Text fontWeight="bold">Rezerwacja</chakra.Text></chakra.TableCell>
+						</chakra.TableRow>
+					</chakra.TableHeader>
+					<chakra.TableBody>
+						{transportData.map(obj => {
+							return renderRow(obj);
+						})}
+					</chakra.TableBody>
+				</chakra.TableRoot>
+			) : (
+				<chakra.AlertRoot status="error">
+					<chakra.AlertIndicator/>
+					<chakra.AlertTitle fontSize="md">Failed to fetch data</chakra.AlertTitle>
+				</chakra.AlertRoot>
+			)}
 		</>
 	);
 }
