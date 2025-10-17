61769a141ccdd03525cbe6550f101cf9ce9fa753
Daniel Wilk <wilkdaniel03@gmail.com>
2025-10-17 21:37:23 +0200

    [web] fixing unwanted alert on success before load

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -2,7 +2,31 @@ import { useContext } from 'react';
 import * as chakra from '@chakra-ui/react';
 import { Heading, Card, Table } from '../components';
 import { dataCtx } from '../DataContext';
-import { FetchStatus } from '../interfaces';
+import { FetchStatus, type IDataContext } from '../interfaces';
+
+const Content = (props: { data: IDataContext }) => {
+	if(props.data.status == FetchStatus.Success) {
+		return (
+			<chakra.TabsRoot defaultValue="reservations">
+				<chakra.TabsList>
+					<chakra.TabsTrigger value="reservations">Reservations</chakra.TabsTrigger>
+					<chakra.TabsTrigger value="employees">Employees</chakra.TabsTrigger>
+					<chakra.TabsTrigger value="vehicles">Vehicles</chakra.TabsTrigger>
+				</chakra.TabsList>
+				<chakra.TabsContent value="reservations">
+					<Table data={props.data.transport}/>
+				</chakra.TabsContent>
+				<chakra.TabsContent value="employees">
+					<Table data={props.data.employee}/>
+				</chakra.TabsContent>
+				<chakra.TabsContent value="vehicles">
+					<Table data={props.data.vehicle}/>
+				</chakra.TabsContent>
+			</chakra.TabsRoot>
+		);
+	}
+
+}
 
 const DashboardPage = () => {
 	const data = useContext(dataCtx);
@@ -10,29 +34,22 @@ const DashboardPage = () => {
 	return (
 		<>
 			<Heading>Panel Administracyjny</Heading>
-				<chakra.Flex justifyContent="space-between" alignItems="start">
-					<chakra.Box w="33%">
-						<Card dotColor={data.transport.length > 0 ? "green.500" : "red.500"} title="Ilość transportów" content={data.transport.length}/>
-					</chakra.Box>
+				<chakra.Flex paddingY="10px" justifyContent="space-between" alignItems="start">
+					<chakra.Flex w="100%" justifyContent="start" alignItems="center">
+						<chakra.Box w="25%">
+							<Card dotColor={data.transport.length > 0 ? "green.500" : "red.500"} title="Ilość transportów" content={data.transport.length}/>
+						</chakra.Box>
+						<chakra.Box paddingLeft="10px" w="25%">
+							<Card dotColor={data.employee.length > 0 ? "blue.500" : "red.500"} title="Ilość pracowników" content={data.transport.length}/>
+						</chakra.Box>
+						<chakra.Box paddingLeft="10px" w="25%">
+							<Card dotColor={data.vehicle.length > 0 ? "orange.500" : "red.500"} title="Ilość pojazdów" content={data.vehicle.length}/>
+						</chakra.Box>
+					</chakra.Flex>
 					<chakra.Button>New reservation</chakra.Button>
 				</chakra.Flex>
-			{data.status == FetchStatus.Success ? (
-				<chakra.TabsRoot defaultValue="reservations">
-					<chakra.TabsList>
-						<chakra.TabsTrigger value="reservations">Reservations</chakra.TabsTrigger>
-						<chakra.TabsTrigger value="employees">Employees</chakra.TabsTrigger>
-						<chakra.TabsTrigger value="vehicles">Vehicles</chakra.TabsTrigger>
-					</chakra.TabsList>
-					<chakra.TabsContent value="reservations">
-						<Table data={data.transport}/>
-					</chakra.TabsContent>
-					<chakra.TabsContent value="employees">
-						<Table data={data.employee}/>
-					</chakra.TabsContent>
-					<chakra.TabsContent value="vehicles">
-						<Table data={data.vehicle}/>
-					</chakra.TabsContent>
-				</chakra.TabsRoot>
+			{data.status != FetchStatus.Failed ? (
+				<Content data={data}/>
 			) : (
 				<chakra.AlertRoot status="error">
 					<chakra.AlertIndicator/>
