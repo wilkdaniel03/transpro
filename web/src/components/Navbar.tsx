a43f69541f1df3970d1d4298f647f35a6e32df87
Daniel Wilk <wilkdaniel03@gmail.com>
2025-10-17 22:14:14 +0200

    [web] displaying logotype in navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import * as chakra from '@chakra-ui/react';
-import Heading from './Heading';
 import NavbarItem from './NavbarItem';
+import Logo from '../../logotype_corrected.webp';
 
 const Navbar = () => {
 	return (
 		<chakra.Flex h="100%" paddingY="5px" flexDirection="column" justifyContent="space-between" alignItems="center">
-			<chakra.Box><Heading>Cos tam transport exclusive</Heading></chakra.Box>
+			<chakra.Box><img src={Logo}/></chakra.Box>
 			<chakra.Box w="100%">
 				<NavbarItem content="Panel Administracyjny" linkPath="/dashboard"/>
 				<NavbarItem content="Powiadomienia" linkPath="/notifications"/>
